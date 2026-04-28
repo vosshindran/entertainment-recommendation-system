a5b607fd658fe0128893ab927c70ee1f0cedc9a8
Vosshindran <vosshen05@gmail.com>
2026-04-28 23:59:18 +0800

    fixed review counting logic for user

--- frontend/js/profile.js
+++ frontend/js/profile.js
@@ -1,20 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const user = storage.getUser();
-    if (!user) {
-        window.location.href = 'login.html';
-        return;
-    }
+  const user = storage.getUser();
+  if (!user) {
+    window.location.href = 'login.html';
+    return;
+  }
 
-    document.getElementById('profile-name').textContent = user.username;
-    document.getElementById('profile-date').textContent = new Date(user.loggedInAt).toLocaleDateString();
+  document.getElementById('profile-name').textContent = user.username;
+  document.getElementById('profile-date').textContent = new Date(
+    user.loggedInAt,
+  ).toLocaleDateString();
 
-    const watchlist = storage.getWatchlist();
-    document.getElementById('watchlist-count').textContent = watchlist.length;
+  const watchlist = storage.getWatchlist();
+  document.getElementById('watchlist-count').textContent = watchlist.length;
 
-    const allReviews = storage.getReviews();
-    let reviewCount = 0;
-    for(const movieId in allReviews) {
-        reviewCount += allReviews[movieId].length;
-    }
-    document.getElementById('reviews-count').textContent = reviewCount;
+  const allReviews = storage.getReviews();
+  let reviewCount = 0;
+  for (const movieId in allReviews) {
+    reviewCount += allReviews[movieId].length;
+  }
+
+  let reviewCount = 0;
+  for (const movieId in allReviews) {
+    const currentMovieReviews = allReviews[movieId]; //all reviews from a single movie
+    const currentUserReviewsForCurrentMovie = currentMovieReviews.filter(
+      (review) => review.author === user.username,
+    );
+  }
+  document.getElementById('reviews-count').textContent = reviewCount;
 });
